01b21166af2752df82355d09b1ba3a41dba46d51
Manvi027 <manvidadhwal@gmail.com>
2025-02-19 12:56:43 +0530

    Connection

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config()
+const {connect} = require('mongoose')
 
 const app = express();
 const port = 3010;
+const db_url = process.env.DB_URl;
 
 app.use(express.static('static'));
+
+const connectToDb = async(url)=>{
+
+  await connect(url)
+
+}
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, async() => {
+  try{
+    await connectToDb(db_url)
+    console.log(`Example app listening at http://localhost:${port}`);
+    console.log("successfully connected DB")
+  }
+  catch(err){
+    console.log(err)
+  }
 });
